46fdff1a951f181a7fb3c38f4f166cac4d2f17e6
Mihai <you@example.com>
2021-05-10 14:24:37 +0300

    Starting teacher interface

--- src/main/java/org/fis/project/controllers/LogInController.java
+++ src/main/java/org/fis/project/controllers/LogInController.java
@@ -1,17 +1,16 @@
 package org.fis.project.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import org.fis.project.Main;
-import org.fis.project.exceptions.CompleteAllFieldsException;
 import org.fis.project.exceptions.CompleteLoginDataException;
-import org.fis.project.exceptions.UsernameAlreadyExistsException;
 import org.fis.project.services.UserService;
-
-import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
@@ -24,6 +23,8 @@ public class LogInController {
     @FXML
     private PasswordField passwordFieldLogin;
 
+
+
     @FXML
     public void switchToRegister() throws Exception {
         Main.setRoot("register");
@@ -35,6 +36,7 @@ public class LogInController {
     }
 
 
+
     @FXML
     public void handleLogIn() {
 
@@ -44,12 +46,19 @@ public class LogInController {
                 loginMessage.setText("You have logged in successfully!");
 
                 Main.setRoot("teacher");
+
+                TeacherController controller=Main.getPath().getController();
+                controller.setHelloMessage("Welcome "+usernameFieldLogin.getText());
+
             }
             else if (UserService.checkCkredentials(usernameFieldLogin.getText(), passwordFieldLogin.getText()).equals("Student")) {
                 loginMessage.setTextFill(Color.web("#008000", 0.8));
                 loginMessage.setText("You have logged in successfully!");
 
                 Main.setRoot("student");
+
+                StudentController controller=Main.getPath().getController();
+                controller.setHelloMessage("Welcome "+usernameFieldLogin.getText());
             }
             else {
                 loginMessage.setTextFill(Color.web("#ef0c0c", 0.8));
