0e3a0ad9cd405d90efe3e7caa77f966398defae9
Mihai <you@example.com>
2021-05-13 00:03:35 +0300

    Load teacher main dashboard from subject information view

--- src/main/java/org/fis/project/controllers/TeacherSubjectsController.java
+++ src/main/java/org/fis/project/controllers/TeacherSubjectsController.java
@@ -1,16 +1,36 @@
 package org.fis.project.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.fis.project.Main;
 
 public class TeacherSubjectsController {
+
+    @FXML
+    private Label helloMessage;
+
+    private static String username;
     @FXML
     public void switchToLogIn() throws Exception {
         Main.setRoot("login");
     }
 
+    public static void setUsername(String username) {
+        TeacherSubjectsController.username = username;
+    }
+
     @FXML
     public void switchToTeacher() throws Exception {
+
         Main.setRoot("teacher");
+
+        TeacherController controller=Main.getPath().getController();
+        controller.setHelloMessage("Welcome "+username);
+        controller.populateDataFromLogIn(username);
+    }
+
+    @FXML
+    public void setHelloMessage(String message) {
+        helloMessage.setText(message);
     }
 }
